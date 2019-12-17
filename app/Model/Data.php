701276a2cac64d230c1060545fd2f5e45e0045d3
Tran Ngoc Duc <caothu91@gmail.com>
2019-12-17 14:03:45 +0700

    bug - sai ten function

--- app/Model/Data.php
+++ app/Model/Data.php
@@ -31,7 +31,7 @@ class Data extends Eloquent {
      * @param string $month
      */
     public function __construct( string $year = null, string $month = null ) {
-        $this->__initialize( $year, $month );
+        $this->initialize( $year, $month );
 
         parent::__construct();
     }
