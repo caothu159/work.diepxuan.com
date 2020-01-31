467fe78ac3f6bf7b9877f98a07614531e33ab48b
Tran Ngoc Duc <caothu91@gmail.com>
2020-01-31 13:38:45 +0700

    sua loi undefined

--- resources/js/components/SalaryComponent.vue
+++ resources/js/components/SalaryComponent.vue
@@ -167,7 +167,7 @@
                 }).then(function(workbook) {
                     window.nhanvienWB = workbook;
                     $.each(XLSX.utils.sheet_to_json(workbook.Sheets.nhanvien), function(keynv, nv) {
-                        self.nhanvien[] = new Nhanvien(nv);
+                        self.nhanvien[nv.__EMPTY] = new Nhanvien(nv);
                     });
                 });
                 /**
