7c7282d32f2806be1f1f4fbc7372d63825c539ac
Tran Ngoc Duc <caothu91@gmail.com>
2020-11-21 18:26:03 +0700

    tao layout moi

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -17,7 +17,7 @@
 @section('sidebar.salary')
     <ul>
         <li>
-            <form method="post" class="mb-1" action="{{ route('salary.import', $time) }}">
+            <form method="post" class="mb-1" action="{{-- route('salary.import', $time) --}}">
                 @method('POST')
                 @csrf
                 <button class="btn btn-link text-left p-0 m-0 border-0 text-decoration-none text-capitalize"
