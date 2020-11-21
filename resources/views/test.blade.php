8d39a61329615efdc052feab1ade682cf2e2f80f
Tran Ngoc Duc <caothu91@gmail.com>
2020-11-21 16:19:57 +0700

    tao layout moi

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,8 +9,8 @@
 
     <div class="col-12">
         <div class="row align-items-stretch salary-container" id="accordionSalary">
-            @include('components/timepicker')
-            @include('salary/salary')
+            <!-- @include('components/timepicker') -->
+            <!-- @include('salary/salary') -->
         </div>
     </div>
 
